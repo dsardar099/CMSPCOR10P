498fd26a1c98056bcbc0fd555fc7c43cc34dc570
Dwaipayan Sardar <dwaipayan.sardar@vitra.ai>
2022-07-09 15:10:43 +0530

    Completed assignment 10

--- Lab/Assignment-10/Assignment10.java
+++ Lab/Assignment-10/Assignment10.java
@@ -1,32 +1,18 @@
-
-// import java.io.FileInputStream;
-// import java.io.FileOutputStream;
-// import java.io.ObjectInputStream;
-// import java.io.ObjectOutputStream;
-// import java.io.IOException;
-// import java.io.Serializable;
 import java.util.Scanner;
-// import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.io.*;
 
-// helper class
 class MyObjectOutputStream extends ObjectOutputStream {
 
-    // Constructor of this class
-    // 1. Default
     MyObjectOutputStream() throws IOException {
-
-        // Super keyword refers to parent class instance
         super();
     }
 
-    // Constructor of this class
-    // 1. Parameterized constructor
     MyObjectOutputStream(OutputStream o) throws IOException {
         super(o);
     }
 
-    // Method of this class
     public void writeStreamHeader() throws IOException {
         return;
     }
@@ -47,11 +33,11 @@ class Student implements Serializable {
 
     @Override
     public String toString() {
-        return "Student{" +
-                "Roll='" + roll + '\'' +
-                ", Name=" + name +
-                ", Subject=" + subject +
-                ", Semester=" + sem +
+        return "Student {" +
+                "Roll:" + roll +
+                ", Name:" + name +
+                ", Subject:" + subject +
+                ", Semester:" + sem +
                 '}';
     }
 }
@@ -81,10 +67,34 @@ class StudentManager {
 
         Student s = new Student(roll, name, subject, sem);
 
-        // File file = new File("studentRecord.dat");
+        if (searchStudent(s.roll) != null) {
+            System.out.println("Student already exists with roll no: " + s.roll);
+            return;
+        }
+
         boolean append = file.exists();
         try {
-            // write object to file
+            FileOutputStream fos = new FileOutputStream(this.file, append);
+            if (file.length() == 0) {
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(s);
+                oos.close();
+            } else {
+                MyObjectOutputStream oos = null;
+                oos = new MyObjectOutputStream(fos);
+                oos.writeObject(s);
+                oos.close();
+            }
+            System.out.println("Student added with roll: " + roll);
+            fos.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addStudent(Student s) {
+        boolean append = file.exists();
+        try {
             FileOutputStream fos = new FileOutputStream(this.file, append);
             if (file.length() == 0) {
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -103,6 +113,10 @@ class StudentManager {
     }
 
     Student searchStudent(int roll) {
+        if (this.file.exists() != true) {
+            System.out.println("Student not found with roll: " + roll);
+            return null;
+        }
         Student s = null;
         try {
             FileInputStream fileIn = new FileInputStream(this.file);
@@ -124,6 +138,10 @@ class StudentManager {
 
     void displayAllStudents() {
         try {
+            if (this.file.exists() != true) {
+                System.out.println("No data found");
+                return;
+            }
             FileInputStream fileIn = new FileInputStream(this.file);
             ObjectInputStream objectIn = new ObjectInputStream(fileIn);
             while (fileIn.available() != 0) {
@@ -132,13 +150,33 @@ class StudentManager {
             }
             objectIn.close();
             fileIn.close();
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
+    Queue<Student> getAllStudent() {
+        Queue<Student> q = new LinkedList<>();
+        try {
+            if (this.file.exists() != true) {
+                return q;
+            }
+            FileInputStream fileIn = new FileInputStream(this.file);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            while (fileIn.available() != 0) {
+                Student std = (Student) objectIn.readObject();
+                q.add(std);
+            }
+            objectIn.close();
+            fileIn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return q;
+    }
+
     void updateStudent() {
+        Queue<Student> students = getAllStudent();
         Scanner in = new Scanner(System.in);
         int roll;
         String name;
@@ -164,28 +202,56 @@ class StudentManager {
         s.name = name;
         s.subject = subject;
         s.sem = sem;
-        try {
-            FileOutputStream fos = new FileOutputStream(this.file, true);
-            MyObjectOutputStream oos = new MyObjectOutputStream(fos);
-            // TODO: update object
-            oos.writeObject(s);
-            oos.close();
-            fos.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        if (this.file.delete()) {
+            System.out.println("Updating...");
+        }
+        while (students.size() != 0) {
+            Student std = students.remove();
+            addStudent(std.roll == roll ? s : std);
+        }
+        System.out.println("Updated...");
+    }
+
+    void deleteStudent() {
+        Queue<Student> students = getAllStudent();
+        Scanner in = new Scanner(System.in);
+        int roll;
+        System.out.println("Enter Student Data");
+        System.out.print("Enter roll: ");
+        roll = Integer.parseInt(in.nextLine());
+        Student s = searchStudent(roll);
+        if (s == null) {
+            System.out.println("Student not found");
+            return;
+        } else {
+            System.out.println("Details of the student:");
+            System.out.println(s);
+            if (this.file.delete()) {
+                System.out.println("Deleting...");
+            }
+            while (students.size() != 0) {
+                Student std = students.remove();
+                if (std.roll != roll) {
+                    addStudent(std);
+                }
+            }
+            System.out.println("Deleted...");
         }
     }
+
 }
 
 public class Assignment10 {
     public static void main(String[] args) {
         StudentManager sm = new StudentManager();
         while (true) {
+            System.out.println("\n****Menu****");
             System.out.println("1. Add Student");
             System.out.println("2. Search Student");
             System.out.println("3. Display All Students");
             System.out.println("4. Update Student");
-            System.out.println("5. Exit");
+            System.out.println("5. Delete Student");
+            System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             Scanner in = new Scanner(System.in);
             int choice = Integer.parseInt(in.nextLine());
@@ -210,6 +276,9 @@ public class Assignment10 {
                     sm.updateStudent();
                     break;
                 case 5:
+                    sm.deleteStudent();
+                    break;
+                case 0:
                     System.exit(0);
                     break;
                 default:
